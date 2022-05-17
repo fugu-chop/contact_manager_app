941e4bd45c644752cc4c0449eb266f56ff89ca3d
Dean Wan <deanwan14@gmail.com>
2022-05-18 08:46:31 +1000

    feat: wiring up controller and model

---
+++ public/javascripts/controller.js
@@ -0,0 +1,4 @@
+// Since we exported an object, we don't have to instantiate 
+import contactModel from './model.js'
+
+console.log(contactModel)
---
+++ public/javascripts/model.js
@@ -0,0 +1,20 @@
+class Model {
+  getAllContacts() {
+    console.log('hello')
+  }
+
+  saveContact(payload) {
+
+  }
+
+  updateContact(id, payload) {
+
+  }
+
+  deleteContact(id) {
+
+  }
+}
+
+// This allows us to directly export an object
+export default new Model()
